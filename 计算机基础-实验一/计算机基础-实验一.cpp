bc0e98fae090257d2144798af8e30a3d28bda1ab
LC-CPU <2464797352@qq.com>
2020-05-22 12:22:34 +0800

    添加项目文件。

---
+++ 计算机基础-实验一/计算机基础-实验一.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<stdio.h>
+int main(void)
+{
+    void check(int*);
+    char a='a';
+    char* b;
+    char c[5] = {'a','n','d','c','v'};
+    int i = 1;
+    int j[5] = {1,2,3,4,5};
+    unsigned int k = 125598;
+    short m = 12;
+    float n = 0.1f;
+    double l = 0.235456;
+    int* d;
+    d=(int*)malloc(sizeof(int));
+    float s = a + i + n;
+    return 0;
+}
+
